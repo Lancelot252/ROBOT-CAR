b193429289fb46be94885bb96204e6c1cd255a6a
RunningKuma <double_kuma@outlook.com>
2024-11-15 18:37:06 +0800

    DOING：尝试接收手机端指令

---
+++ ros_connect/build/robot_control/CMakeFiles/control_listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/pi/ros_connect/devel/lib/robot_control/control_listener"
+  "/home/pi/ros_connect/devel/lib/robot_control/control_listener.pdb"
+  "CMakeFiles/control_listener.dir/src/control_listener.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/control_listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
