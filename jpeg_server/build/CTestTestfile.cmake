4d99deb1c49729777ff9b39feb056ef872f254e9
RunningKuma <double_kuma@outlook.com>
2025-03-11 09:36:09 +0800

    完成小车手机端控制，尝试兼容新摄像头

--- jpeg_server/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/ROBOT-CAR/jpeg_server/src
-# Build directory: /home/pi/ROBOT-CAR/jpeg_server/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("jpeg_server")
--- jpeg_server/build/jpeg_server/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/ROBOT-CAR/jpeg_server/src/jpeg_server
-# Build directory: /home/pi/ROBOT-CAR/jpeg_server/build/jpeg_server
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
