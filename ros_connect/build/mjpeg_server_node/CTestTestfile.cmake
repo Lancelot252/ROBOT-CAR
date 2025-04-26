22ea977c1758228ef1d3eab4210c08fa0e049b5d
RunningKuma <double_kuma@outlook.com>
2025-04-26 22:20:06 +0800

    优化项目结构，整合了软件包

--- ros_connect/build/mjpeg_server_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/ROBOT-CAR/ros_connect/src/mjpeg_server_node
-# Build directory: /home/pi/ROBOT-CAR/ros_connect/build/mjpeg_server_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros_connect/build/udp_server/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/ROBOT-CAR/ros_connect/src/udp_server
-# Build directory: /home/pi/ROBOT-CAR/ros_connect/build/udp_server
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
